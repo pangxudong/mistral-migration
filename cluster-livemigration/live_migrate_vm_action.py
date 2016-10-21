c515ad515ef4fce33e5f96550fffa9f18b43f47d
pangxudong <pangxudong0312@gmail.com>
2016-10-21 11:49:51 +0800

    update

--- cluster-livemigration/live_migrate_vm_action.py
+++ cluster-livemigration/live_migrate_vm_action.py
@@ -4,15 +4,15 @@ from mistral.workflow.utils import Result
 
 class LiveMigrateVmAction(NovaAction):
 
-    def __init__(self, uuid, host, live_migrate, block_migration, disk_over_commit):
+    def __init__(self, uuid, host, migrate, block_migration, disk_over_commit):
         self._uuid = uuid
         self._host = host
-        self._live_migrate = live_migrate
+        self._migrate = migrate
         self._block_migration = block_migration
         self._disk_over_commit = disk_over_commit
 
     def run(self):
         client = self._get_client()
 
-        if self._live_migrate:
+        if self._migrate:
             client.servers.live_migrate(self._uuid, host=self._host, block_migration=self._block_migration, disk_over_commit=self._disk_over_commit)
